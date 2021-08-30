ec2db6ae34cbf06bba6af62a9377f22e69c8e967
abains-aj <66849032+abains-aj@users.noreply.github.com>
2021-08-30 16:11:13 -0700

    Create DropoutPlotly.R

---
+++ DropoutPlotly.R
@@ -0,0 +1,17 @@
+library(plotly)
+df <- read.csv(file.choose(),header = TRUE) #choose DeltaDropoutPlotly.csv: plot input file with ID number, ORF 1 CT, and N CT value
+
+fig <- df %>%
+  plot_ly(
+    type = 'scatter',
+    mode = 'markers',
+    x = ~N_CTValue,
+    y = ~ORFab_CTValue,
+    text = ~ID,
+    marker = list(),
+    hovertemplate =paste("<b>%{text}</b><br><br>",
+                         "%{yaxis.title.text}: %{y:}<br>",
+                         "%{xaxis.title.text}: %{x:}<br>",
+                         "<extra></extra>")) 
+
+fig
