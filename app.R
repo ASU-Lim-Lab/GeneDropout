9d96f654bf2b28306ed646958a9cec26657cd26d
abains-aj <66849032+abains-aj@users.noreply.github.com>
2021-09-14 16:08:46 -0700

    Update app.R

--- app.R
+++ app.R
@@ -27,6 +27,15 @@ ui <- fluidPage(
     
     selectInput('xcol', 'X Variable', ""),
     selectInput('ycol', 'Y Variable', "", selected = ""),
+    br(),
+    p("Click on 'Browse' to upload an input file with sample IDs as the first column and CT values as the rest of the columns.Ensure that all CT value columns contain only numbers"),
+    HTML("<p><a href='https://github.com/ASU-Lim-Lab/GeneDropout/blob/main/DeltaDropoutPlotly.csv'>Example Input File</a></p>"),
+    p("Under the 'Separator', select whether your input file is a comma, tab, or semicolon separated file"),
+    p("Select the X and Y variables to be plotted. The options will show the header names of the input file"),
+    p("Click on the 'Plot' tab to display interactive plot of samples IDs and CT values"),
+    p("Click on 'Download Plotly Graph' to save copy of graph locally"),
+    br(),
+
   ),
   
   mainPanel(
@@ -46,7 +55,8 @@ ui <- fluidPage(
                    titlePanel(""),
                    verbatimTextOutput("")
                  )
-    )
+    ),
+    
   )
 )
 
@@ -85,8 +95,8 @@ server <- function(input, output, session) {
   output$MyPlot <- renderPlotly({
     x <- data()[, c(input$xcol, input$ycol)]
     
-    p <- ggplot(x, aes_string(input$xcol, input$ycol,label = factor(data()[, 1])))
-    p <- p + geom_point()+theme_bw()
+    p <- ggplot(x, aes_string(input$xcol, input$ycol,Label = factor(data()[, 1])))
+    p <- p + geom_point()+theme_bw() 
     plotly_p <- plotly::ggplotly(p)
     plotly_p
     session_store$plt <- plotly_p
